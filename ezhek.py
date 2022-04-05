739bf51aecdcb22622f63fb71e7065e505765206
xseven <99634076+xseven-gt@users.noreply.github.com>
2022-04-05 12:40:44 +0700

    Create ezhek.py

---
+++ ezhek.py
@@ -0,0 +1,59 @@
+import time
+
+import os
+
+import colorsys
+
+from colorama import Fore, Back, Style
+
+colorama.init()
+
+import colorama
+
+os.system("color C")
+
+print("Hechking Akun Xenzaxnxx18++")
+
+time.sleep(3)
+
+print("Meowbah IP : 124.36.269.82")
+
+time.sleep(3)
+
+print("Meowbah Email : xenzapedo@gmail.com")
+
+time.sleep(3)
+
+print("Meowbah Password : XenzaPedo133")
+
+time.sleep(3)
+
+os.system("color B")
+
+print(" [XNXX] HECKING PRO WIBU MENU HECKING [XNXX] ")
+
+print(" ")
+
+print(" ")
+
+print(" 1. Login [XenzaXnxx] Discord ")
+
+print(" 2. Attack [XenzaPedo] IP ")
+
+print(" 3. Hengking [XenzaXXX] Account ")
+
+os.system("color C")
+
+print(" Your Choice : ")
+
+print(" ")
+
+print(" HENGKING XENZA ACCOUNT ")
+
+print(" ")
+
+print(" Login Xenza18+ Pro Max Account... ")
+
+print(" ")
+
+print(" Succes Hecking Account... ")
